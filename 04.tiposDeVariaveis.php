a990131f00f54a52eb103f453ebfd6fed0c63b80
Manoelllima <manoelllima@github.com>
2025-07-21 14:42:27 -0300

    tipos de Variaveis

---
+++ 04.tiposDeVariaveis.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Titulo</title>
+    </head>
+    <body>
+        <?php
+          
+            $nome = "Manoel"; //tipo texto(String)
+            $idade = 28; // numero (int)
+            $numero = true; // true/false (boolean)
+            $peso = 66.4; //numero fracionado (double)
+            echo "Minha idade é ".$idade."<br>"; //concatenar utiliza o ponto.
+            echo 'Meu nome é '.$nome."<br>";
+            echo 'Meu peso é '.$peso."<br>";
+        ?>
+    </body>
+</html>
