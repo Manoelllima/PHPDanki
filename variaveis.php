8068952eb32be8be748232b3865974781e9d1a76
Manoelllima <manoelllima@github.com>
2025-07-20 21:41:39 -0300

    Variaveis

---
+++ variaveis.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $peso; // Variavel não inicializada 
+            $nome = "Manoel"; //Varivael com valor atribuido
+            $idade = 28;
+            $numero = $idade; // Variaveis podem receber valores de outras variaveis
+            
+            echo "<h1>$nome $idade $dev $peso $numero</h1>";
+        ?>
+    </body>
+</html>
