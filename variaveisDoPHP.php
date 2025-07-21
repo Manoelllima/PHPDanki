11fee9f5d7d32a002a95a6dcd3e514dbf65d83bc
Manoelllima <manoelllima@github.com>
2025-07-20 21:24:11 -0300

    Server variables

---
+++ variaveisDoPHP.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //Server Variables https://www.php.net/manual/pt_BR/reserved.variables.server.php
+            echo $_SERVER['SERVER_NAME'];
+            echo $_SERVER['SERVER_ADMIN'];
+            echo $_SERVER['DOCUMENT_ROOT'];
+        ?>
+    </body>
+</html>
