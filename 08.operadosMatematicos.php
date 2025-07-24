f46dc99a87cac0259467ebf586bc51f80917a52e
Manoelllima <manoelllima@github.com>
2025-07-24 18:54:58 -0300

    Operadores matematicos

---
+++ 08.operadosMatematicos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $numUm = 10;
+            $numDois = 20;
+            $soma = $numUm + $numDois;
+            
+            echo $soma."<br>"; // soma
+            echo $numUm * $numDois."<br>"; //multiplicação
+            echo $numDois / $numUm."<br>"; //divisão
+            echo (2+47) / 2 ;   // () maior ordem de precedencia
+            echo "<br>";
+            echo 3%2; // modulo, resto da divisão
+        ?>
+    </body>
+</html>
